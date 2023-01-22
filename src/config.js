360f8c3a3d078ef790a1260d6578b09fa5256e19
Malshan-svg <62680809+Malshan-svg@users.noreply.github.com>
2023-01-22 20:34:52 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -5,5 +5,5 @@
  */
 
 // get token here https://beta.openai.com/account/api-keys
-API_KEY = ["SET_TOKEN_HERE"]; // can multiple token
+API_KEY = ["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"]; // can multiple token
 URL_PROFILE_INSTAGRAM = "https://instagram.com/theazran_";
